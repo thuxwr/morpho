81dcd74f8dce0d09d10921f7f29d05d6cfbd9077
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2018-01-24 17:12:42 -0800

    Initial addition of dungfly

---
+++ dungfly/__init__.py
@@ -0,0 +1,10 @@
+'''
+'''
+
+from __future__ import absolute_import
+
+import pkg_resources
+__version__ = pkg_resources.require("morpho")[0].version.split('-')[0]
+__commit__ = pkg_resources.require("morpho")[0].version.split('-')[-1]
+
+from . import Axon
